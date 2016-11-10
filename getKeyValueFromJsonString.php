0f553259a014556184e16c20c55fa435121cb7d0
Steve Binder <email@stevebinder.com>
2016-11-10 17:50:46 -0500

    Create getKeyValueFromJsonString.php

---
+++ getKeyValueFromJsonString.php
@@ -0,0 +1,32 @@
+// Grabs the value for a specific key in a json structure.
+// This can be up to 100 times faster than using json_decode
+// and then doing a lookup on the key.
+// The closer the key is to the beginning of the string
+// the faster the lookup will be.
+function getKeyValueFromJsonString($json, $key) {
+    $matches;
+    preg_match("/\"".$key."\":(\"?(.*?[^\\\\])?\"?),/", $json, $matches);
+    $value = $matches ? $matches[1]: "";
+    $first = substr($value, 0, 1);
+    if ($first === "\"") {
+        return preg_replace('/\\\"/', "\"", substr($value, 1, strlen($value) - 2));
+    }
+    else if ($first === "{" || $first === "[") {
+        return $value;
+    }
+    else if ($value === "true") {
+        return true;
+    }
+    else if ($value === "false") {
+        return false;
+    }
+    else if ($value === "null") {
+        return null;
+    }
+    else if ($value === "undefined") {
+        return undefined;
+    }
+    else {
+        return $value * 1;
+    }
+}
