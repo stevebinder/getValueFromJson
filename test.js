e7fa3d2e81fe523352353229cf40d647ca5c75a3
Steve Binder <email@stevebinder.com>
2016-11-10 20:07:31 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+<?php
+
+require("json.php");
+
+$sample = '{"first":"hello", "second":true, "third":"cat\"s long yarn", "fourth":22}';
+
+var_dump(array(
+    "get" => json::get($sample, "first"),
+    "set" => json::set($sample, "first", array("mark" => "Jones")),
+    "remove" => json::remove($sample, "third"),
+    "add_end" => json::add($sample, "xyz", "cat"),
+    "add_begin" => json::add($sample, "xyz", "dog", true),
+));
+
+
+?>
