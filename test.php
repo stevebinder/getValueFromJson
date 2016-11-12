7f87803726163f1c9ca966e7699e5c8b8788158c
Steve Binder <email@stevebinder.com>
2016-11-12 12:37:24 -0500

    Update test.php

--- test.php
+++ test.php
@@ -3,40 +3,34 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-require("../../common/json.php");
+require("json.php");
 
-$sample = '{"first":"hello","second":true,"third":"cat\"s long yarn","fourth":22,"fifth":"value"}';
+$sample1 = '{"one":1,"two":"two","three":true,"four":0.11,"five":25,"six":6, "seven":"my \"quoted\" string\""}';
+$sample2 = '{"one":1,"two":"name"}';
 
-var_dump(array(
-    "get" => json::get($sample, "first"),
-    "set" => json::set($sample, "third", "supe\"fish"),
-    "remove_third" => json::remove($sample, "third"),
-    "remove_fourth" => json::remove($sample, "fourth"),
-    "remove_fifth" => json::remove($sample, "fifth"),
-    "add_end" => json::add($sample, "xyz", "cat"),
-    "add_begin" => json::add($sample, "xyz", "dog", true),
-));
+$results = array(
+    "get_1" => json::get($sample1, "one") === 1,
+    "get_2" => json::get($sample1, "two") === "two",
+    "get_3" => json::get($sample1, "three") === true,
+    "get_4" => json::get($sample1, "four") === 0.11,
+    "get_5" => json::get($sample1, "five") === 25,
+    "get_6" => json::get($sample1, "six") === 6,
+    "get_7" => json::get($sample1, "seven") === "my \"quoted\" string\"",
+
+    "set_1" => json::set($sample2, "one", 2) === '{"one":2,"two":"name"}',
+    "set_2" => json::set($sample2, "one", "let\"s go") === '{"one":"let\"s go","two":"name"}',
+    "set_3" => json::set($sample2, "two", "let\"s go") === '{"one":1,"two":"let\"s go"}',
+    "set_4" => json::set($sample2, "two", 0.25) === '{"one":1,"two":0.25}',
+    "set_5" => json::set($sample2, "two", true) === '{"one":1,"two":true}',
+
+    "remove_1" => json::remove($sample2, "one") === '{"two":"name"}',
+    "remove_2" => json::remove($sample2, "two") === '{"one":1}',
+    "remove_3" => json::remove($sample2, array("two", "one")) === '{}',
+);
 
 
-$sample = '{"third":"33"}';
-
-var_dump(array(
-    "get" => json::get($sample, "first"),
-    "set" => json::set($sample, "third", 33),
-    "remove_third" => json::remove($sample, "third"),
-    "add_end" => json::add($sample, "xyz", "cat"),
-    "add_begin" => json::add($sample, "xyz", "dog", true),
-));
-
-
-$sample = '{"first":"hello","second":true,"third":"cat\"s long yarn","fourth":22,"fifth":"value"}';
-
-var_dump(array(
-    "get" => json::get($sample, array("first", "second")),
-    "set" => json::set($sample, array("first" => "111", "second" => "222")),
-    "remove" => json::remove($sample, array("first", "second")),
-    "add_end" => json::add($sample, array("a"=>1, "b"=>2)),
-    "add_begin" => json::add($sample, array("a"=>1, "b"=>2), true),
-));
+foreach ($results as $key => $value) {
+    echo $key."[".($value ? "PASS" : "FAIL")."]\n";
+}
 
 ?>
