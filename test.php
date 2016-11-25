aa8bfd7c41ded41a3019bd71b4805a6828bb9665
Steve Binder <email@stevebinder.com>
2016-11-24 22:27:20 -0500

    Update test.php

--- test.php
+++ test.php
@@ -15,6 +15,14 @@ $sample7 = '{}';
 $sample8 = '[]';
 $sample9 = '[2,3]';
 $sample10 = '{"a":{}}';
+$sample11 = '{"a":{"one":1,"two":2}}';
+$sample12 = '{"a":{"one":1,"two":2},"b":{"three":3,"four":4}}';
+$sample13 = '{"a":"a \" ] complex { string"}';
+$sample14 = '{"a":{"one":{"x":"y}"}}}';
+$sample15 = '{"a":"i am \"a string\""}';
+$sample16 = '{"a":null,"b":2}';
+$sample17 = '{"b":{"a":3},"a":1,"c":{"a":2}}';
+$sample18 = '{"a":"this\" me"}';
 
 function test($output, $correct) {
     $result = $output === $correct ? "PASS" : "FAIL";
@@ -34,6 +42,13 @@ $results = array(
     "get_10" => test(json::get($sample6, "a"), "{}"),
     "get_11" => test(json::get($sample6, "b"), "[]"),
     "get_12" => test(json::get($sample6, "c"), ""),
+    "get_13" => test(json::get($sample13, "a"), "a \" ] complex { string"),
+    "get_14" => test(json::get($sample14, "a"), '{"one":{"x":"y}"}}'),
+    "get_15" => test(json::get($sample15, "a"), 'i am "a string"'),
+    "get_16" => test(json::get($sample16, "a"), null),
+    "get_17" => test(json::get($sample17, "a"), 1),
+    "get_18" => test(json::get($sample18, "a"), 'this" me'),
+    
 
     "set_1" => test(json::set($sample2, "one", 2), '{"one":2,"two":"name"}'),
     "set_2" => test(json::set($sample2, "one", "let\"s go"), '{"one":"let\"s go","two":"name"}'),
@@ -58,12 +73,15 @@ $results = array(
     "set_21" => test(json::set($sample6, "a", array(1, 2)), '{"a":[1,2],"b":[],"c":""}'),
     "set_22" => test(json::set($sample6, "a", '{"name":"of"}'), '{"a":{"name":"of"},"b":[],"c":""}'),
     "set_23" => test(json::set($sample10, "a", '{"c":"d"}'), '{"a":{"c":"d"}}'),
+    "set_24" => test(json::set($sample11, "a", 'hello'), '{"a":"hello"}'),
     
     "remove_1" => test(json::remove($sample2, "one"), '{"two":"name"}'),
     "remove_2" => test(json::remove($sample2, "two"), '{"one":1}'),
     "remove_3" => test(json::remove($sample2, array("two", "one")), '{}'),
     "remove_4" => test(json::remove($sample4, "b"), '{"a":"ok","c":"name"}'),
     "remove_5" => test(json::remove($sample4, "a"), '{"b":"my \n\n word\"s here","c":"name"}'),
+    "remove_6" => test(json::remove($sample14, "a"), '{}'),
+    "remove_7" => test(json::remove($sample6, "b"), '{"a":{},"c":""}'),
 
     "add_1" => test(json::add($sample7, "cat"), '{}'),
     "add_2" => test(json::add($sample8, "cat"), '["cat"]'),
